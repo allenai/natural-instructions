67d07afa9845fd1bd1b58a418901e145f7d3d45e
Daniel Khashabi <danielk@allenai.org>
2021-06-10 15:13:30 -0700

    update the test file.

--- src/test_all.py
+++ src/test_all.py
@@ -21,10 +21,21 @@ for file in files:
         file_path = tasks_path + file
         with open(file_path, 'r') as f:
             data = json.load(f)
-            # for key in expected_keys:
-            #     assert key in data, f'did not find the key: {key}'
+            for key in expected_keys:
+                assert key in data, f'did not find the key: {key}'
 
-            # for x in data['Instances']:
-            #     assert type(x['input']) == str, f'the input of instance {x} is not a string'
-            #     assert type(x['output']) == list, f'the output of instance {x} is not a list'
-                # assert type(x['output']) == list, f'the output of instance {x} is not a list'
+            for x in data['Instances']:
+                for key in ['input', 'output']:
+                    assert key in x, f'expected the key {key} in {x}'
+                assert type(x['input']) == str, f'the input of instance {x} is not a string'
+                assert type(x['output']) == list, f'the output of instance {x} is not a list'
+                for i in x['output']:
+                    assert type(i) == str, f'the output is not a string'
+
+            for x in data['Positive Examples'] + data['Negative Examples']:
+                for key in ['input', 'output', 'explanation']:
+                    assert key in x, f'expected the key {key} in {x}'
+                assert type(x['input']) == str, f'the input of example {x} is not a string'
+                assert type(x['output']) == str, f'the output of example {x} is not a string'
+                assert type(x['explanation']) == str, f'the explanation of example {x} is not a string'
+
