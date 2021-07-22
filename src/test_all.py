4326b95ea4250d95673b4938fa95407fbc279d6c
Daniel Khashabi <danielk@allenai.org>
2021-07-22 13:22:36 -0700

    make sure that the task name is mentioned in the readme file.

--- src/test_all.py
+++ src/test_all.py
@@ -14,6 +14,9 @@ expected_keys = [
     'Categories'
 ]
 
+with open("tasks/README.md", 'r') as readmef:
+    task_readme_content = readmef.readlines()
+
 files = [f for f in listdir(tasks_path) if isfile(join(tasks_path, f))]
 for file in files:
     print(f" --> testing file: {file}")
@@ -25,8 +28,10 @@ for file in files:
             for key in expected_keys:
                 assert key in data, f'did not find the key: {key}'
 
-            assert len(data['Instances']) > 25, f"there must be at least 25 instances; currently you have {len(data['Instances'])} instances"
-            assert len(data['Instances']) <= 6500, f"there must be at most 6.5k instances; currently you have {len(data['Instances'])} instances"
+            assert len(data[
+                           'Instances']) > 25, f"there must be at least 25 instances; currently you have {len(data['Instances'])} instances"
+            assert len(data[
+                           'Instances']) <= 6500, f"there must be at most 6.5k instances; currently you have {len(data['Instances'])} instances"
 
             for x in data['Instances']:
                 for key in ['input', 'output']:
@@ -52,8 +57,9 @@ for file in files:
                 for y_idx in range(x_idx + 1, len(data['Instances'])):
                     y = data['Instances'][x_idx]
                     if x['input'] == y['input']:
-                        assert f" * Looks like we have a repeated example here! :-/ \n {x}\n {y}"
+                        raise Exception(f" * Looks like we have a repeated example here! :-/ \n {x}\n {y}")
 
-
+            if file not in task_readme_content:
+                raise Exception(f' * looks like the task name `{file}` is not included in the task file `tasks/README.md`')
 
 print("Did not find any errors! ✅")
