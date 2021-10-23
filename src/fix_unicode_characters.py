153ae4a9b685eb7560cb496f5dbd2cfcefa5894a
Pegah Alipoormolabashi <palipoor976@gmail.com>
2021-10-22 18:09:36 -0700

    Add a file to fix unicode characters in files

---
+++ src/fix_unicode_characters.py
@@ -0,0 +1,26 @@
+import json
+import os
+from os import listdir, path
+from os.path import isfile, join
+
+"""
+Script for fixing unicode characters in task files
+"""
+
+tasks_path = 'tasks/'
+# modify these variables as needed
+def fix_characters(tasks_path):
+    files = [join(tasks_path, f) for f in listdir(tasks_path) if isfile(join(tasks_path, f))]
+    files.sort()
+    # add the domain
+    for file in files: 
+        if file.endswith('.json'):
+            print(file)
+            with open(file) as f:
+                data = json.load(f)
+            with open(file, 'w') as o:
+                dump = json.dumps(data, ensure_ascii = False, indent = 4)
+                o.write(dump)
+
+if __name__ == "__main__":
+    fix_characters(tasks_path)
