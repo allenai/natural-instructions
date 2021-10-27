8486c9e6a77440bff1eccfe4945ad55164504495
Daniel Khashabi <danielk@allenai.org>
2021-10-27 12:46:28 -0700

    -

--- eval/amt/2.run_eval.sh
+++ eval/amt/2.run_eval.sh
@@ -1,12 +1,10 @@
 # launch the experiment
 export AWS_PROFILE=alexandria-mturk
-amti --verbose create-batch mturk-specs/definition-likert file_name.jsonl . --live
-# for example,
-# amti --verbose create-batch mturk-specs/definition-likert start=100_end=200_max_size=5.jsonl .
+amti --verbose create-batch mturk-specs/definition-likert start=301_end=400_max_size=5.jsonl . --live
 
 # check the experiment status
 export BATCH=batch-...
-#export BATCH=batch-3aa8d2c2-484b-460d-b43c-98668d54a9b2_start=101_end=118_max_size=5
+#export BATCH=batch-fc02d066-1e35-4184-b4ea-ba7eca1abcc1_start=301_end=400_max_size=5
 amti status-batch "$BATCH" --live
 
 # fetch the results when they're over
--- eval/amt/3.amti_aggregate_results.py
+++ eval/amt/3.amti_aggregate_results.py
@@ -33,6 +33,7 @@ def discretize(x):
 
 def aggregate_v2(response_file):
     worker_stats = {}
+    suggestions =  {}
     with open(response_file) as f:
         for line in f.readlines():
             json_line = json.loads(line)
@@ -68,32 +69,48 @@ def aggregate_v2(response_file):
                     negative_examples.append(json_line[id])
             negative_examples_appended = normalize("//".join(negative_examples))
 
+            instruction_suggestions = instruction_suggestions.replace("\n", " ")
+            positive_ex_suggestions = positive_ex_suggestions.replace("\n", " ")
+            negative_ex_suggestions = negative_ex_suggestions.replace("\n", " ")
+
             prefix = f"{instruction_quality_value}\t{instruction_suggestions}\t{file}\t{instructions}" \
                      f"\t{positive_ex_quality_value}\t{positive_ex_suggestions}\t{positive_examples_appended}" \
                      f"\t{negative_ex_quality_value}\t{negative_ex_suggestions}\t{negative_examples_appended}"
 
-            instance_input = []
-            instance_output = []
-            instance_prediction = []
+            if len(instruction_suggestions) + len(positive_ex_suggestions) + len(negative_ex_suggestions) > 20:
+                if file not in suggestions:
+                    suggestions[file] = []
+                suggestions[file].append(f"\n    - regarding instructions: `{instruction_suggestions}`"
+                                         f"\n    - regarding p examples: `{positive_ex_suggestions}`"
+                                         f"\n    - regarding n examples: `{negative_ex_suggestions}`\n")
+            # instance_input = []
+            # instance_output = []
+            # instance_prediction = []
             for idx in range(0, 5):
                 id = f'instance_{idx}_input'
                 if id in json_line:
                     input = normalize(json_line[id])
                     output = normalize(json_line[f'instance_{idx}_output'])
                     human_output = normalize(json_line[f'annotated_instance_{idx}_output'])
-                    instance_input.append(input)
-                    instance_output.append(output)
-                    instance_prediction.append(human_output)
+                    # instance_input.append(input)
+                    # instance_output.append(output)
+                    # instance_prediction.append(human_output)
                     rouge_val = metric_max_over_ground_truths(
                         rouge, normalize2(human_output),
                         [normalize2(x) for x in output.split("///")]
                     )
                     print(f"{prefix}\t{input}\t{output}\t{human_output}\t{rouge_val}\t{worker_id}")
 
-        print(worker_stats)
+    for file in sorted(suggestions.keys()):
+        print(f" - [ ] {file}")
+        for feedback in suggestions[file]:
+            print(feedback)
+
 
 
 # aggregate_v2("batch-43ecd7ef-0717-4b72-a39c-b6179f8b5f77_task156_pilot/batch-results.jsonl")
 # aggregate_v2("batch-65c49abc-f8a3-4f12-a71d-9ce5742c3419_start=60_end=100_max_size=5/batch-results.jsonl")
 # aggregate_v2("batch-eea0ef32-da0a-47cf-bcae-810d1a503379_start=1_end=59_max_size=5/batch-results.jsonl")
-aggregate_v2("batch-cdcb497e-49a3-4cee-8ab5-1451dc19dac2_119_end=200_max_size=5/batch-results.jsonl")
+# aggregate_v2("batch-cdcb497e-49a3-4cee-8ab5-1451dc19dac2_119_end=200_max_size=5/batch-results.jsonl")
+# aggregate_v2("batch-58086c69-62bf-4e91-8741-b68d27e1fd63-start=201_end=300_max_size=5/batch-results.jsonl")
+aggregate_v2("batch-fc02d066-1e35-4184-b4ea-ba7eca1abcc1_start=301_end=400_max_size=5/batch-results.jsonl")
